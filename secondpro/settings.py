cc956ba32116394fa2aa78aefcfb3a8b0694e071
unknown <yurida1666@gmail.com>
2019-07-31 19:11:37 +0900

    a

--- secondpro/settings.py
+++ secondpro/settings.py
@@ -15,7 +15,6 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -144,6 +143,7 @@ MEDIA_URL='/media/'
 
 
 # Heroku: Update database configuration from $DATABASE_URL.
+
 
 
 
